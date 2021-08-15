fd89d18643553bff7b179c53f61f93d40770a80f
kkojae <jaejeung3210@gmail.com>
2021-08-15 15:33:50 +0900

    js 산술, 할당 연산자

---
+++ part03_jsEssentials/ch2_jsStarter/산술_할당연산자.js
@@ -0,0 +1,22 @@
+// 산술 연산자 (arithmetic operator)
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
+
+// 할당 연산자 (assignment operator)
+// const는 재할당이 불가능
+// let은 재할당이 가능
+const a = 2;
+
+let b = 2;
+// b = b + 1;
+// 위의 코드를 숏컷으로 작성할 수 있다.
+b += 1;
+b -= 1;
+b *= 2;
+b /= 2;
+b %= 1;
+
+console.log(b);

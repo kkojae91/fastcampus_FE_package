1fd63f2f81e885f67dab8b082deb46916d37ca18
kkojae <jaejeung3210@gmail.com>
2021-08-15 15:55:31 +0900

    js 삼항 연산자

---
+++ part03_jsEssentials/ch2_jsStarter/삼항연산자.js
@@ -0,0 +1,13 @@
+// 삼항 연산자(ternary operator)
+
+const a = 1 < 2;
+
+if (a) {
+  console.log("참");
+} else {
+  console.log("거짓");
+}
+
+// 삼항연산자 숏컷
+a ? console.log("참") : console.log("거짓");
+console.log(a ? "참" : "거짓");
